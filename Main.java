d6d3764297c70d7371aa96fc9d8d9fab83e1b392
Rothesa Arcena <rothesav.arcena@gmail.com>
2025-10-01 01:56:31 +0800

    Update Main.java
    added character and game mode input scanner

--- Main.java
+++ Main.java
@@ -21,6 +21,8 @@ public class Main{
         System.out.println("  ╚═══════════════════════════════════════════════════════════════════════════════╝");
        
         System.out.println();
+        System.out.println("Press ENTER to begin...");
+        sc.nextLine();
         System.out.println();
 
         System.out.println("     ************************************************************************");
@@ -38,10 +40,33 @@ public class Main{
         System.out.println();
         System.out.println();
         System.out.println();
+
+        //choose gamemode
         System.out.println("Choose your gamemode:");
         System.out.println("1. Player vs Player (PVP)");
         System.out.println("2. Player vs Computer (PVC)");
         System.out.println("3. Arcade Mode");
+        System.out.print("Enter choice (1-3): ");
+        int mode = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println();
+        System.out.println("You choose  mode " + mode + ".");
+        System.out.println();
+
+        //choose characters
+        System.out.println("Choose Your Character:");
+        System.out.println("1. Jollibee - Crispy Joy Bringer");
+        System.out.println("2. McDo - Big Mac Basher");
+        System.out.println("3. KFC - Colonel’s Drumstick Duelist");
+        System.out.println("4. Burger King - Flame Griller");
+        System.out.println("5. Wendy’s - Sassy Red Fighter");
+        System.out.println("6. Jack in the Box - Trickster Clown");
+        System.out.println("7. Little Caesars - Pizza Gladiator");
+        System.out.println("8. Chief Khai (Kuya Guard) - Whistle Warrior");
+        System.out.print("Enter choice (1-8): ");
+        int character = scan.nextInt();
+        scan.nextLine();
 
 
     }
