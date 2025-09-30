af6179f2cb3f592e3441150dbf42a3a080156f3f
ZaydAbdullah <akishima2006@gmail.com>
2025-09-30 12:44:05 +0800

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("      *     *   *****   *****   *****   *****   *****   ");
+        System.out.println("      **   **   *   *   *       *      *     *    *     ");
+        System.out.println("      * * * *   *****   *****   *      *     *    *     ");
+        System.out.println("      *  *  *   *   *       *   *      *     *    *     ");
+        System.out.println("      *     *   *   *   *****   *****   *****     *     ");
+        System.out.println();
+        System.out.println("****   *****   *   *  ******   ****   *****   *   *   ******    ");
+        System.out.println("*   *  *   *   **  *  *        *   *  *   *   **  *   *         ");
+        System.out.println("****   *****   * * *  *  ***   ****   *****   * * *   *  ***    ");
+        System.out.println("*   *  *   *   *  **  *    *   *   *  *   *   *  **   *    *    ");
+        System.out.println("****   *   *   *   *  ******   ****   *   *   *   *   ******    ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println("*****************************************************************");
+        System.out.println("*        Welcome the player to Battle of the Mascots,           *");
+        System.out.println("*     a turn-based RPG-style game where restaurant mascots      *");
+        System.out.println("*    and special characters battle using food-themed attacks.   *");
+        System.out.println("*****************************************************************");
+        System.out.println();
+        System.out.println();
+        System.out.println("Goal: Reduce your opponent’s HP to 0 using strategy, skills, and combos.");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Choose your gamemode: ");
+        System.out.println("Player vs Player (PVP)");
+        System.out.println("Player vs Computer (PVC)");
+        System.out.println("Arcade Mode");
+
+
+    }
+}
