1998ea1a4e3ebab7097690218d20135f1302061a
Rothesa Arcena <rothesav.arcena@gmail.com>
2025-10-01 21:57:47 +0800

    CHECK IF INVALID INPUT
    CHECKS THE INPUT FOR GAME MODE IF IT IS INVALID OR NOT

--- temp.java
+++ temp.java
@@ -1,7 +1,7 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
-public class Main{
+public class temp{
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
@@ -71,29 +71,42 @@ public class Main{
         }
 
         System.out.println();
-        System.out.println("You choose mode " + mode + ".");
+        System.out.println("You choose  mode " + mode + ".");
         System.out.println();
 
-        //START GAME
-        //Game game = new Game();
-        switch(mode) {
-            case 1:
-                System.out.println("--------------------Starting PVP MODE--------------------");
-                //game.startPVP();  // start PVP mode
-                break;
-            case 2:
-                System.out.println("--------------------Starting PVC MODE--------------------");
-                //game.startPVC();  // start Player vs Computer
-                break;
-            case 3:
-                System.out.println("--------------------Starting ARCADE MODE--------------------");
-                //game.startArcade();  // start Arcade Mode
-                break;
-            default:
-                System.out.println("Invalid mode selected!");
+        //choose characters
+        int character_choice;
+        while(true) {
+            System.out.println("Choose Your Character:");
+            System.out.println("1. Jollibee - Crispy Joy Bringer");
+            System.out.println("2. McDo - Big Mac Basher");
+            System.out.println("3. KFC - Colonel’s Drumstick Duelist");
+            System.out.println("4. Burger King - Flame Griller");
+            System.out.println("5. Wendy’s - Sassy Red Fighter");
+            System.out.println("6. Jack in the Box - Trickster Clown");
+            System.out.println("7. Little Caesars - Pizza Gladiator");
+            System.out.println("8. Chief Khai (Kuya Guard) - Whistle Warrior");
+            System.out.print("Enter choice (1-8): ");
+
+            if(scan.hasNextInt()){
+                character_choice = scan.nextInt();
+
+                if (character_choice >= 1 && character_choice <= 3) {
+                    break;
+                } else {
+                    System.out.println();
+                    System.out.println("Invalid number! Please enter between 1 and 8.");
+                    System.out.println();
+                }
+            } else {
+                System.out.println();
+                System.out.println("Invalid input! Numbers only.");
+                System.out.println();
+                scan.next();
+            }
+
         }
 
-        scan.close();
 
     }
 }
