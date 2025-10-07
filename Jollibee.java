53c95bc1e0c1d8dcaa7ba69135d53f23a0943e9b
wengGitH <wengie.arriola@cit.edu>
2025-10-07 11:56:34 +0800

    All Characters Added (stats not yet final)

--- Jollibee.java
+++ Jollibee.java
@@ -1,20 +1,24 @@
 public class Jollibee extends Character{
-    public Jollibee() {
+    public Jollibee() 
+    {
         super("Jollibee", 120, 80, 15); //character name, hp, mana, mana regen
     }
 
     @Override
-    public void basicAttack(Character target) {
+    public void basicAttack(Character target) 
+    {
         performAttack(target, 10, 15, 0, "basic"); //target name, min dmg, max dmg, mana cost
     }
 
     @Override
-    public void skillAttack(Character target) {
+    public void skillAttack(Character target) 
+    {
         performAttack(target, 20, 25, 20, "skill"); //target name, min dmg, max dmg, mana cost
     }
 
     @Override
-    public void ultimateAttack(Character target) {
+    public void ultimateAttack(Character target) 
+    {
         performAttack(target, 40, 50, 50, "ult"); //target name, min dmg, max dmg, mana cost
     }
     
---
+++ Wendys.java
@@ -0,0 +1,26 @@
+public class Wendys extends Character{
+    public Wendys() 
+    {
+        super("Wendys", 120, 80, 15); //character name, hp, mana, mana regen
+    }
+
+    @Override
+    public void basicAttack(Character target) 
+    {
+        performAttack(target, 10, 15, 0, "basic"); //target name, min dmg, max dmg, mana cost
+    }
+
+    @Override
+    public void skillAttack(Character target) 
+    {
+        performAttack(target, 20, 25, 20, "skill"); //target name, min dmg, max dmg, mana cost
+    }
+
+    @Override
+    public void ultimateAttack(Character target) 
+    {
+        performAttack(target, 40, 50, 50, "ult"); //target name, min dmg, max dmg, mana cost
+    }
+    
+    
+}
