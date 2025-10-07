b0f677f6bf8f7db1d22a2b05cb1be8b6e41897cf
wengGitH <wengie.arriola@cit.edu>
2025-10-07 11:49:26 +0800

    Jollibee and McDonalds.java extends Character

---
+++ McDonalds.java
@@ -0,0 +1,22 @@
+public class McDonalds extends Character{
+    public McDonalds() {
+        super("McDonalds", 120, 80, 15); //character name, hp, mana, mana regen
+    }
+
+    @Override
+    public void basicAttack(Character target) {
+        performAttack(target, 10, 15, 0, "basic"); //target name, min dmg, max dmg, mana cost
+    }
+
+    @Override
+    public void skillAttack(Character target) {
+        performAttack(target, 20, 25, 20, "skill"); //target name, min dmg, max dmg, mana cost
+    }
+
+    @Override
+    public void ultimateAttack(Character target) {
+        performAttack(target, 40, 50, 50, "ult"); //target name, min dmg, max dmg, mana cost
+    }
+    
+    
+}
