e4563de58fc221b99dc06af523a249566259ab54
Rothesa Arcena <rothesav.arcena@gmail.com>
2025-10-27 18:42:07 +0800

    SAMPLE BATTLE CLASS
    SAMPLE BATTLE CLASS PLEASE EDIT THIS dili pa final

--- battle/Battle.java
+++ battle/Battle.java
@@ -12,8 +12,59 @@ public class Battle {
         this.player2 = player2;
     }
 
-
     public void start() {
+        System.out.println(" Battle Start! ");
+        System.out.println(player1.getName() + " vs " + player2.getName());
+        System.out.println("----------------------------------");
 
+        // Battle loop
+        while (player1.isAlive() && player2.isAlive()) {
+            playerTurn(player1, player2);
+            if (!player2.isAlive()) break;
+
+            playerTurn(player2, player1);
+        }
+
+        System.out.println("----------------------------------");
+        if (player1.isAlive()) {
+            System.out.println( player1.getName() + " wins the battle!");
+        } else {
+            System.out.println(player2.getName() + " wins the battle!");
+        }
+    }
+
+    private void playerTurn(Character attacker, Character defender) {
+        System.out.println(attacker.getName() + "'s turn!");
+        System.out.println("Choose an action:");
+        System.out.println("1. Basic Attack");
+        System.out.println("2. Secondary Skill");
+        System.out.println("3. Ultimate Skill");
+
+        int choice;
+        while (true) {
+            System.out.print("Enter choice: ");
+            try {
+                choice = Integer.parseInt(scan.nextLine());
+                if (choice >= 1 && choice <= 3) break;
+                System.out.println(" Invalid choice! Try again.");
+            } catch (Exception e) {
+                System.out.println(" Please enter a number!");
+            }
+        }
+
+        switch (choice) {
+            case 1:
+                attacker.basicAttack(defender);
+                break;
+            case 2:
+                attacker.secondarySkill(defender);
+                break;
+            case 3:
+                attacker.ultimateSkill(defender);
+                break;
+        }
+
+        System.out.println(defender.getName() + " HP: " + defender.getHealth());
+        System.out.println(attacker.getName() + " HP: " + attacker.getHealth());
     }
 }
